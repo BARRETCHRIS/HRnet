08e793209a112022a86f29bc4d11ebd4fbcad63d
Crazy Beasty <christelle.barret.poirot@gmail.com>
2024-09-26 15:10:36 +0200

    fix nav

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,15 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './header.scss';
 
 function Header(){
+    const location = useLocation();
+
     return (
         <header className='header'>
             <Link to="/" className='header_title'><h1 className='header_title_h1'>HRnet</h1></Link>
             <nav className='header_nav'>
-                <Link to="/create">Create</Link>
-                <Link to="/list">View List</Link>
+                <Link className={`${location.pathname === '/create' ? 'active' : ''}`} to="/create">Create</Link>
+                <Link className={`${location.pathname === '/list' ? 'active' : ''}`} to="/list">View List</Link>
             </nav>
         </header>
     )
