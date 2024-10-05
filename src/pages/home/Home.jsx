b24ccf319e6c642f4c7d3cf8a41f17eaed835f43
Crazy Beasty <christelle.barret.poirot@gmail.com>
2024-10-05 02:32:19 +0200

    fix performance and add reports

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,19 +7,19 @@ const Home = React.memo(() => {
 
     return (
         <main className="wrapper">
-            <div className="home">
-                <div className="home_content">
+            <section className="home">
+                <article className="home_content">
                     <h2>Welcome Wealth Health HRnet</h2>
                     <p className='home_content_txt'>
                         This is the employee management app. <br />
                         Please choose your action.
                     </p>
-                    <div className='home_content_actions'>
-                        <Link className='home_content_actions_btn' to='/create'>Create Employee</Link>
-                        <Link className='home_content_actions_btn' to='/list'>View list Employee</Link>
-                    </div>
-                </div>
-            </div>
+                </article>
+                <article className='home_actions'>
+                    <Link className='home_actions_btn' to='/create'>Create Employee</Link>
+                    <Link className='home_actions_btn' to='/list'>View list Employee</Link>
+                </article>
+            </section>
         </main>
     );
 });
